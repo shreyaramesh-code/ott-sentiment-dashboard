e6874bda186fb593c565a5853b43e9e3dad4c192
shreyaramesh-code <shreya.ramesh@mba.christuniversity.in>
2026-02-12 20:37:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ st.markdown("AI-Augmented Market & Audience Signal System")
 @st.cache_data
 def load_data():
     df = pd.read_csv("youtube_master_clean.csv")
-    st.write("DEBUG – Columns in deployed dataset:")
-    st.write(df.columns.tolist())
-    return df
 
     # Auto-detect sentiment column
     sentiment_col = None
@@ -172,4 +169,3 @@ with tab4:
 
         fig5 = px.line(volume_trend, x="date", y="Volume")
         st.plotly_chart(fig5, use_container_width=True)
-
